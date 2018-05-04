4b2d37b1b29139997bdc5bf6e2f9eaead8fca96a
samarthbhargav <samarth.bhargav92@gmail.com>
2018-05-04 19:07:50 +0200

    Finished Skipgram (I think)

--- practical-2/data.py
+++ practical-2/data.py
@@ -122,6 +122,8 @@ class UnigramSamplingTable:
 
     def remove_word(self, word, sample=0.001):
         z_wi = self.freq[word]/self.num_tokens
+        if z_wi == 0:
+            return False
         prob = ((math.sqrt(z_wi / sample) + 1)) * ((sample)/z_wi)
         return prob < random.random()
 
